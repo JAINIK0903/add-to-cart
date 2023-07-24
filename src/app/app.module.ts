3759b44873eb8157c7c96a4772c41c94ffde4395
Jainik090302 <git@github.com:Jainik090302/online-shoping.git>
2023-07-24 09:58:38 +0530

    implement filter functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,22 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
 import { CartComponent } from './component/cart/cart.component';
 import { ProductComponent } from './component/product/product.component';
+import { FilterPipe } from './shared/filter.pipe';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     CartComponent,
-    ProductComponent
+    ProductComponent,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/component/product/product.component.ts
+++ src/app/component/product/product.component.ts
@@ -9,16 +9,34 @@ import { CartService } from 'src/app/service/cart.service';
 })
 export class ProductComponent {
   public productList: any;
+  public filterCategory: any;
+  public searchKey: string = ''
   constructor(private apiService: ApiService, private cartService: CartService) { }
   ngOnInit() {
     this.apiService.getProduct().subscribe(res => {
       this.productList = res;
+      this.filterCategory = res;
       this.productList.forEach((a: any) => {
+        if (a.category === "women's clothing" || a.category === "men's clothing") {
+          a.category = "fashion"
+        }
         Object.assign(a, { quantity: 1, total: a.price });
       });
+      console.log(this.productList);
+    })
+    this.cartService.search.subscribe((val: any) => {
+      this.searchKey = val;
     })
   }
   addToCart(item: any) {
     this.cartService.addToCart(item);
+  }
+  filter(category: string) {
+    this.filterCategory = this.productList
+      .filter((a: any) => {
+        if (a.category == category || category == '') {
+          return a;
+        }
+      })
   }
 }
